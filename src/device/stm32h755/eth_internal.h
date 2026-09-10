34b6b2ed9cf0a013ba544d37bb35007de6e48f3d
Robert A Incze <98164670+Inczert@users.noreply.github.com>
2026-09-11 00:54:37 +0200

    feat(DAS): add STM32H755 Ethernet handle

---
+++ src/device/stm32h755/eth_internal.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef DAS_STM32H755_ETH_INTERNAL_H
+#define DAS_STM32H755_ETH_INTERNAL_H
+
+#include <das/eth.h>
+
+typedef enum stm32h755_eth_instance {
+    STM32H755_ETH1 = 1
+} stm32h755_eth_instance_t;
+
+/** Construct a generic DAS handle for a supported STM32H755 Ethernet MAC. */
+das_eth_t stm32h755_eth_handle(stm32h755_eth_instance_t instance);
+
+#endif

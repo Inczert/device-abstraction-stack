92514f6cecae44a2a0a8e542504f0c5d5969f0d2
Robert A Incze <98164670+Inczert@users.noreply.github.com>
2026-09-11 00:32:21 +0200

    test(DAS): cover umbrella public header

--- tests/compile/public_headers.c
+++ tests/compile/public_headers.c
@@ -1,19 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
-#include <das/board.h>
-#include <das/board_resources.h>
-#include <das/cache.h>
-#include <das/clock.h>
+#include <das/das.h>
 #include <das/cortex_m/startup.h>
-#include <das/dma.h>
-#include <das/gpio.h>
-#include <das/i2c.h>
-#include <das/irq.h>
-#include <das/result.h>
-#include <das/spi.h>
-#include <das/time.h>
-#include <das/timer.h>
-#include <das/uart.h>
 
 int main(void) {
     return 0;
